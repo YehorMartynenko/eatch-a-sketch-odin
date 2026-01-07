c0b91a0dfc81767387e683bd288a3542562e92d9
YehorMartynenko <yehor.martynenko.27@gmail.com>
2026-01-07 08:12:10 +0100

    Add change grid size functionality

--- js/script.js
+++ js/script.js
@@ -1,23 +1,43 @@
 const container = document.querySelector("#container");
+let gridSize = 16;
 
-for(let i = 0; i<11; i++){
-    const row = document.createElement("div");
-    row.classList.add("row");
-    container.appendChild(row);
-    for(let j = 0; j<11; j++){
-        const grid = document.createElement("div");
-        grid.classList.add("gridElement");
-        row.appendChild(grid);
+function drawGrid(size){
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    for(let i = 0; i<size; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        container.appendChild(row);
+        for(let j = 0; j<size; j++){
+            const grid = document.createElement("div");
+            grid.classList.add("gridElement");
+            row.appendChild(grid);
+        }
     }
 }
 
-const gridElements = document.querySelectorAll(".gridElement");
-console.log(gridElements);
-gridElements.forEach((el) => {
-    el.addEventListener("mouseenter", function(e) {
-        this.style.backgroundColor = getRandomColor();
+drawGrid(gridSize);
+setDrawMode();
+
+const sizeSelector = document.querySelector("#size");
+sizeSelector.addEventListener("click", function(e){
+    gridSize = prompt('Select grid size(from 4 to 100)');
+    if(gridSize<4) gridSize=4;
+    if(gridSize>100) gridSize=100;
+    drawGrid(gridSize);
+    setDrawMode();
+});
+
+function setDrawMode() {
+    const gridElements = document.querySelectorAll(".gridElement");
+    console.log(gridElements);
+    gridElements.forEach((el) => {
+        el.addEventListener("mouseenter", function(e) {
+            this.style.backgroundColor = getRandomColor();
+        })
     })
-})
+}
 
 function getRandomColor(){
     let range = "0123456789ABCDEF";
@@ -30,5 +50,5 @@ function getRandomColor(){
     }
     console.log("#" + color.join(""));
 
-    return "#" + color.join("");;
+    return "#" + color.join("");
 }
